74b687315e50e00d5530819edf2129f042370e4c
Xareni Reyes Soto <reyessoto.xareni@gmail.com>
2025-02-25 17:21:06 +0100

    First commit

---
+++ find_pockets.py
@@ -0,0 +1,31 @@
+import sys
+from math import sqrt
+from statistics import mean
+from itertools import combinations
+
+def PDB_iterator(pdb_file_path=None):
+    if pdb_file_path is None:
+        fi = sys.stdin
+    else:
+        fi = open(pdb_file_path,"r")
+
+    for line in fi:
+        line = line.strip()
+
+        record_name = line[0:6].strip()
+
+        if (record_name == "ATOM"):
+            serial_number = int(line[6:11].strip())
+            atom_name = line[12:16].strip()
+            residue_name = line[17:20].strip()
+            chain_ID = line[21].strip()
+            residue_ID = int(line[22:26].strip())
+            x_coordinate = float(line[30:38].strip())
+            y_coordinate = float(line[38:46].strip())
+            z_coordinate = float(line[46:54].strip())
+            yield(serial_number, atom_name, residue_name, chain_ID, residue_ID, x_coordinate, y_coordinate, z_coordinate)
+
+    fi.close()
+
+def define_box():
+    pass
